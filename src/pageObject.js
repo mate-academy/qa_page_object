8683d7dfe77bcf4c240329bde8bf930fd4de0b1f
Taisiia <taisiia.moshkatiuk@gmail.com>
2023-02-21 15:52:26 +0200

    Solution

--- src/pageObject.js
+++ src/pageObject.js
@@ -1,15 +1,37 @@
 'use strict';
 
-class Header {
+const { Linter } = require('eslint');
 
+class Header {
+  get logo() {
+    return 'Conduit logo';
+  }
 };
 
 class PageObject {
-
+  constructor(url) {
+    this.url = url;
+    this.header = new Header();
+  }
+  clickOnLogo() {
+    return `Click on the ${this.header.logo}`;
+  }
 };
 
 class ArticlePage extends PageObject {
-
+  constructor(url, slug) {
+    super(url);
+    this.url += slug;
+  }
+  get commentButton() {
+    return '[Publish comment] button';
+  }
+  clickOnCommentButton() {
+    return 'Click on the' + this.clickOnCommentButton;
+  }
+  assertPageOpened() {
+    return 'The' + this.url + 'is opened';
+  }
 };
 
 module.exports = {
