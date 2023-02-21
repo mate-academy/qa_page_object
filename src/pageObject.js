3af55ae6ab916373eb4d6763f82c64ce395908ed
Andrii Ivanov <ai830539@gmail.com>
2023-02-21 20:20:33 +0200

    try to push task

--- src/pageObject.js
+++ src/pageObject.js
@@ -1,15 +1,39 @@
 'use strict';
 
 class Header {
-
+ get logo() {
+  return "Conduit logo"
+ }
 };
 
-class PageObject {
+class PageObject extends Header {
+  constructor(url){
+  super(logo);
+  this.url = url;
+  this.header = Header();
+  }
 
+  clickOnLogo() {
+    return `Click on the ${this.logo}`
+  }
 };
 
 class ArticlePage extends PageObject {
+  constructor(url,slug){
+    PageObject.url = url;
+    this.slug = slug
+  }
 
+  get commentButton() {
+    return `${this.url} + ${this.slug}`;
+  }
+
+  clickOnCommentButton() {
+    return `Click on the ${this.commentButton}`;
+  }
+  assertPageOpened() {
+    return `The ${this.url} is opened`;
+  }
 };
 
 module.exports = {
