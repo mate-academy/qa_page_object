8c6bb2dfd0710d4481aa020ff2f12f6044bda2d3
Taisiia <taisiia.moshkatiuk@gmail.com>
2023-03-08 11:08:02 +0200

    Solution

--- src/pageObject.js
+++ src/pageObject.js
@@ -11,7 +11,6 @@ class PageObject {
     this.url = url;
     this.header = new Header();
   }
-
   clickOnLogo() {
     return `Click on the ${this.header.logo}`;
   }
@@ -26,10 +25,10 @@ class ArticlePage extends PageObject {
     return '[Publish comment] button';
   }
   clickOnCommentButton() {
-    return 'Click on the ${this.commentButton}';
+    return 'Click on the' + this.commentButton;
   }
   assertPageOpened() {
-    return 'The ${this.url} is opened';
+    return 'The' + this.url + 'is opened';
   }
 };
 
