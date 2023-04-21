77741e4bf7ac285cc43b9dd7260d0527355514d3
Artem <protiv55@gmail.com>
2023-04-21 11:39:56 +0300

    Solution

--- src/pageObject.test.js
+++ src/pageObject.test.js
@@ -1,86 +1,39 @@
 'use strict';
 
-const { Header, PageObject, ArticlePage } = require('./pageObject');
+class Header {
+  get logo() {
+    return 'Conduit logo';
+  }
+};
 
-describe('Header class', () => {
-  it('should create an instance of Header', () => {
-    const header = new Header();
+class PageObject {
+  constructor(url) {
+    this.header = new Header();
+    this.url = url;
+  }
+  clickOnLogo() {
+    return `Click on the ${this.header.logo}`;
+  }
+};
 
-    expect(header)
-      .toBeInstanceOf(Header);
-  });
+class ArticlePage extends PageObject {
+  constructor(url, slug) {
+    super(url);
+    this.slug = slug;
+  }
+  get commentButton() {
+    return '[Publish comment] button';
+  }
+  clickOnCommentButton() {
+    return `Click on the ${this.commentButton}`;
+  }
+  assertPageOpened() {
+    return `The ${this.url} is opened`;
+  }
+};
 
-  it('should contain a logo element', () => {
-    const header = new Header();
-
-    expect(header.logo)
-      .toBe('Conduit logo');
-  });
-});
-
-describe('PageObject class', () => {
-  it('should create an instance of PageObject', () => {
-    const page = new PageObject('http://test.com');
-
-    expect(page)
-      .toBeInstanceOf(PageObject);
-  });
-
-  it('should contain a url property', () => {
-    const page = new PageObject('http://test.com');
-
-    expect(page.url)
-      .toBe('http://test.com');
-  });
-
-  it('should contain a header element as an instanse of Header', () => {
-    const page = new PageObject('http://test.com');
-
-    expect(page.header)
-      .toBeInstanceOf(Header);
-  });
-
-  it('should contain a "clickOnLogo" method', () => {
-    const page = new PageObject('http://test.com');
-
-    expect(page.clickOnLogo())
-      .toBe(`Click on the ${page.header.logo}`);
-  });
-});
-
-describe('ArticlePage class', () => {
-  it('should create an instance of ArticlePage', () => {
-    const page = new ArticlePage('http://test.com', '/test-slug');
-
-    expect(page)
-      .toBeInstanceOf(ArticlePage);
-  });
-
-  it('should extend the PageObject class', () => {
-    const page = new ArticlePage('http://test.com', '/test-slug');
-
-    expect(page)
-      .toBeInstanceOf(PageObject);
-  });
-
-  it('should contain a "commentButton" property', () => {
-    const page = new ArticlePage('http://test.com', '/test');
-
-    expect(page.commentButton)
-      .toBe(`[Publish comment] button`);
-  });
-
-  it('should contain a "clickOnCommentButton" method', () => {
-    const page = new ArticlePage('http://test.com', '/test');
-
-    expect(page.clickOnCommentButton())
-      .toBe(`Click on the ${page.commentButton}`);
-  });
-
-  it('should contain a "assertPageOpened" method', () => {
-    const page = new ArticlePage('http://test.com', '/test');
-
-    expect(page.assertPageOpened())
-      .toBe(`The ${page.url} is opened`);
-  });
-});
+module.exports = {
+  Header,
+  PageObject,
+  ArticlePage,
+};
