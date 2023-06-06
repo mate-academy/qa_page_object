ec1e31a00d5e53ff46028495bd23a9c6a0d57d43
Kateryna Yatsenko <k.yatsenko5@gmail.com>
2023-06-06 17:27:22 +0200

    added semicolon

--- src/pageObject.js
+++ src/pageObject.js
@@ -3,18 +3,18 @@
 class Header {
   get logo() {
     return `Conduit logo`;
-  }
+  };
 };
 
 class PageObject {
   constructor(url) {
     this.header = new Header();
     this.url = url;
-  }
+  };
 
   clickOnLogo() {
     return `Click on the ${this.header.logo}`;
-  }
+  };
 };
 
 class ArticlePage extends PageObject {
@@ -22,19 +22,19 @@ class ArticlePage extends PageObject {
     super(url);
     this.slug = slug;
     this.url = url + slug;
-  }
+  };
 
   get commentButton() {
     return '[Publish comment] button';
-  }
+  };
 
   clickOnCommentButton() {
     return `Click on the ${this.commentButton}`;
-  }
+  };
 
   assertPageOpened() {
     return `The ${this.url} is opened`;
-  }
+  };
 };
 
 module.exports = {
