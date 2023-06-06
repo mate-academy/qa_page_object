8a06e614ea8997c4ad961b416a16d784bfeecc0c
Anton Zaluzhnyi <133892122+antonzaluzhnuy@users.noreply.github.com>
2023-06-06 13:29:11 +0300

    Solution Update pageObject.js
    Solution

--- src/pageObject.js
+++ src/pageObject.js
@@ -1,15 +1,34 @@
 'use strict';
 
 class Header {
-
+get logo() {
+  return 'Conduit logo';
+  }
 };
 
 class PageObject {
-
+constructor(url) {
+  this.url = url;
+  this.heder = new Header();
+  }
+  clickOnLogo() {
+  return `Click on the ${Header.logo}`}
 };
 
 class ArticlePage extends PageObject {
-
+constructor(url,slug) {
+  super(url);
+    this.url = url + slug;
+  };
+ get commentButton() {
+  return '[Publish comment] button';
+  }
+  clickOnCommentButton() {
+  return `Click on the ${this.commentButton}`;
+  }
+  assertPageOpened() {
+  return `The ${this.url} is opened`;
+  }
 };
 
 module.exports = {
