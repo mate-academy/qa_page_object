da316ddeb7a42b31f2b4d2fafe85fe94d3075899
Olesia Semen <semenolesa01@gmail.com>
2023-11-04 11:48:18 +0200

    Testing

--- src/pageObject.js
+++ src/pageObject.js
@@ -1,16 +1,40 @@
 'use strict';
 
 class Header {
+  get logo() {
+    return 'Conduit logo';
+  }
 
-};
+  clickOnLogo() {
+    return 'Click on the ' + this.logo;
+  }
+}
+
 
 class PageObject {
+  constructor(url) {
+    this.url = url;
+    this.header = new Header();
+  }
+}
 
-};
+class ArticlePage {
+  constructor(url, slug) {
+    this.url = url + '/' + slug;
+  }
 
-class ArticlePage extends PageObject {
+  get commentButton() {
+    return '[Publish comment] button';
+  }
 
-};
+  clickOnCommentButton() {
+    return 'Click on the ' + this.commentButton;
+  }
+
+  assertPageOpened() {
+    return 'The ' + this.url + ' is opened';
+  }
+}
 
 module.exports = {
   Header,
