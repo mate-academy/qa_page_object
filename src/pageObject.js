d900b3c814dcab5dc07abbceb223f46ded5bafa2
OlgaBelmega <143718672+OlgaBelmega@users.noreply.github.com>
2024-01-05 17:43:58 +0200

    Update page

--- src/pageObject.js
+++ src/pageObject.js
@@ -1,15 +1,34 @@
 'use strict';
 
 class Header {
-
+  get logo() {
+    return 'Conduit logo';
+  }
 };
 
 class PageObject {
-
+  constructor(url, header) {
+    this.url = 'http://test.com';
+  }
+  clickOnLogo() {
+    console.log(`Click on the ${this.logo}`);
+  }
 };
 
 class ArticlePage extends PageObject {
-
+  constructor(url, slug) {
+    super(url);
+    this.newUrl = this.url + slug;
+  }
+  get commentButton() {
+    return '[Publish comment] button';
+  }
+  clickOnCommentButton() {
+    return 'Click on the ' + this.commentButton;
+  }
+  assertPageOpened() {
+    return 'The ' + this.url + ' is opened';
+  }
 };
 
 module.exports = {
