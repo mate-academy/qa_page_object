4bbc6529f951bac88de3609bfff0c723b0a0a7f0
Lyubov Nekroyenko <nekroyenko@gmail.com>
2024-01-07 21:12:14 +0200

    add classes

--- src/pageObject.js
+++ src/pageObject.js
@@ -1,15 +1,43 @@
+/* eslint-disable no-this-before-super */
+/* eslint-disable no-useless-constructor */
+/* eslint-disable indent */
+/* eslint-disable no-undef */
 'use strict';
 
 class Header {
-
+  constructor() {
+    }
+  get logo() {
+    return `Conduit logo`;
+  }
 };
 
 class PageObject {
-
+  constructor(url) {
+    this.url = url;
+    this.header = new Header();
+  }
+  clickOnLogo() {
+    return `Click on the ${this.header.logo}`;
+  }
+  assertPageOpened() {
+    return `The ${this.url} is opened`;
+}
 };
-
 class ArticlePage extends PageObject {
+constructor(url, slug) {
+  super(url + '/' + slug);
 
+    this.slug = slug;
+}
+
+get commentButton() {
+    return '[Publish comment] button';
+}
+
+clickOnCommentButton() {
+    return `Click on the ${this.commentButton}`;
+}
 };
 
 module.exports = {
