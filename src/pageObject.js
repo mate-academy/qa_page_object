7c59d19f2993f30b98dc319d294bd1bfb59dadbc
gajdziormax@gmail.com <gajdziormax@gmail.com>
2024-04-30 13:37:35 +0200

    pageObject task

--- src/pageObject.js
+++ src/pageObject.js
@@ -1,15 +1,35 @@
 'use strict';
 
 class Header {
+  constructor(logo){
+    this.logo = 'Conduit logo';
+    return logo
+  }
 
 };
 
 class PageObject {
+  constructor(headerlogo, url){
+    this.header = new Header(headerlogo);
+    this.url = url;
+  }
+  clickOnLogo(){
+    return 'Click on the' + this.headerlogo
+  }
 
 };
 
 class ArticlePage extends PageObject {
-
+  constructor(url, slug){
+    this.url = url + slug
+    this.commentButton = '[Publish comment] button'
+  }
+  clickOnTheCommentButton(){
+    return 'Click on the' + this.commentButton
+  }
+  assertPageOpened(){
+    return 'The' + this.url + 'is opened'
+  }
 };
 
 module.exports = {
