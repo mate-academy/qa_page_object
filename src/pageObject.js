ce74ecb41444d9387af78d2cdbe16179413f085d
ElaOz <ozga.elzbieta94@gmail.com>
2024-05-01 22:26:28 +0200

    solution to task

--- src/pageObject.js
+++ src/pageObject.js
@@ -1,15 +1,45 @@
 'use strict';
 
 class Header {
-
+  get logoText() {
+    return 'Conduit logo';
+  }
 };
 
 class PageObject {
+  constructor(url) {
+    if (typeof url !== 'string') {
+      throw new Error('URL must be a string');
+    }
+    this.url = url;
+    this.header = new Header();
+  }
 
-};
+  clickOnLogo() {
+    return 'Click on the ' + this.header.logoText;
+  }
+}
 
 class ArticlePage extends PageObject {
+  constructor(url, slug) {
+    super(url);
+    if (typeof slug !== 'string') {
+      throw new Error('Slug must be a string');
+    }
+    this.url += slug;
+  }
 
+  get commentButton() {
+    return '[Publish comment] button';
+  }
+
+  clickOnCommentButton() {
+    return 'Click on the ' + this.commentButton;
+  }
+
+  assertPageOpened() {
+    return 'The ' + this.url + ' is opened';
+  }
 };
 
 module.exports = {
