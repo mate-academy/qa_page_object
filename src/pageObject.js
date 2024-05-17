834110a27061a54b3b4d213b6ce4dfb1b84119fc
Kristina Iechkalova <kristina.echkalova@gmail.com>
2024-05-17 13:00:20 +0200

    Solution

--- src/pageObject.js
+++ src/pageObject.js
@@ -3,37 +3,33 @@
 class Header {
   get logo() {
     return 'Conduit logo';
-  };
+  }
 };
 
 class PageObject {
   constructor(url) {
     this.url = url;
     this.header = new Header();
-  };
-
+  }
   clickOnLogo() {
-    return `Click on the ${this.header.logo}`;
-  };
+    return 'Click on the ' + this.header.logo;
+  }
 };
 
 class ArticlePage extends PageObject {
   constructor(url, slug) {
     super(url);
-    this.slug = slug;
-  };
-
+    this.url = url + slug;
+  }
   get commentButton() {
     return '[Publish comment] button';
-  };
-
+  }
   clickOnCommentButton() {
-    return `Click on the ${this.commentButton}`;
-  };
-
+    return 'Click on the ' + this.commentButton;
+  }
   assertPageOpened() {
-    return `The ${this.url} is opened`;
-  };
+    return 'The ' + this.url + ' is opened';
+  }
 };
 
 module.exports = {
