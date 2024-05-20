076ea80b36d02367c625ab2ebf4945ccbe85fece
Maria <maria.szynaka@gmail.com>
2024-05-20 23:46:46 +0200

    Solution

---
+++ cypress.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  e2e: {
+    baseUrl: 'https://conduit.mate.academy/',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+};
--- src/pageObject.js
+++ src/pageObject.js
@@ -1,16 +1,40 @@
 'use strict';
 
 class Header {
-
-};
+  get logo() {
+    return 'Conduit logo'; // Zwraca string zamiast selektora
+  }
+}
 
 class PageObject {
+  constructor(url) {
+    this.url = url;
+    this.header = new Header();
+  }
 
-};
+  clickOnLogo() {
+    return `Click on the ${this.header.logo}`; // Zwraca string zamiast wykonywać akcję
+  }
+}
 
 class ArticlePage extends PageObject {
+  constructor(url, slug) {
+    super(url); // Wywołanie konstruktora klasy nadrzędnej
+    this.url = url + slug;
+  }
 
-};
+  get commentButton() {
+    return '[Publish comment] button'; // Zwraca string zamiast selektora
+  }
+
+  clickOnCommentButton() {
+    return `Click on the ${this.commentButton}`; // Zwraca string zamiast wykonywać akcję
+  }
+
+  assertPageOpened() {
+    return `The ${this.url} is opened`; // Zwraca string zamiast używać console.log
+  }
+}
 
 module.exports = {
   Header,
