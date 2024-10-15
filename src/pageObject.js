3d8614ed1b8ea6699fb7297872d1641d4acc1c78
Yuliia Dovbysh <juli3stepura@gmail.com>
2024-10-15 17:22:41 +0300

    Solution2

--- src/pageObject.js
+++ src/pageObject.js
@@ -4,28 +4,30 @@ class Header {
   get logo() {
     return 'Conduit logo';
   }
-};
+}
 
 class PageObject {
   constructor(url) {
     this.url = url;
     this.header = new Header();
   }
+
+  // Placeholder method for future functionality
   clickOnLogo() {
     return `Click on the ${this.header.logo}`;
   }
-};
+}
 
 class ArticlePage extends PageObject {
   constructor(url, slug) {
-    super(url);
-    this.url = url + slug;
+    super(url + slug);
   }
 
   get commentButton() {
     return '[Publish comment] button';
-  };
+  }
 
+  // Placeholder method for future functionality
   clickOnCommentButton() {
     return `Click on the ${this.commentButton}`;
   }
@@ -33,7 +35,7 @@ class ArticlePage extends PageObject {
   assertPageOpened() {
     return `The ${this.url} is opened`;
   }
-};
+}
 
 module.exports = {
   Header,
