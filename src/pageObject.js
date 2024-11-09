5d1eb818ddcd78dfde32eaa1803f94bd6e7aee2f
Demian Zolotukhin <demianzolotukhin@gmail.com>
2024-11-09 20:34:13 +0200

    solution

--- src/pageObject.js
+++ src/pageObject.js
@@ -1,15 +1,42 @@
 'use strict';
 
 class Header {
-
+  logo() {
+    return 'Conduit logo';
+  }
 };
 
 class PageObject {
+  constructor(url, slug) {
+    this.url = url + slug;
+    this.header = new Header();
+  }
 
+  visit() {
+    cy.visit(this.url); 
+  }
+
+  clickOnLogo() {
+    return `Click on the ${this.header.logo()}`;
+  }
 };
 
 class ArticlePage extends PageObject {
+  constructor(url, slug) {
+    super(url, slug);
+  }
 
+  commentButton() {
+    return cy.get('[Publish comment] button');
+  }
+
+  clickOnCommentButton() {
+    return `Click on the ${this.commentButton()}`;
+  }
+
+  assertPageOpened() {
+    return `The ${url} is opened`;
+  }
 };
 
 module.exports = {
