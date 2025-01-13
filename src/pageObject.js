ed7c8dd7e8005210faf35c2fddbe39efb9f8abeb
Galers <galosaandrew@gmail.com>
2025-01-13 13:06:21 +0200

    test: Add tests

--- src/pageObject.js
+++ src/pageObject.js
@@ -1,16 +1,29 @@
 'use strict';
 
 class Header {
-
-};
+  logo = 'Conduit logo';
+}
 
 class PageObject {
+  url = 'http://test.com';
 
-};
+  header = new Header();
+
+  clickOnLogo() {
+    return `Click on the ${this.header.logo}`;
+  }
+}
 
 class ArticlePage extends PageObject {
-
-};
+  url = 'http://test.com/article';
+  commentButton = '[Publish comment] button';
+  clickOnCommentButton() {
+    return `Click on the ${this.commentButton}`;
+  }
+  assertPageOpened() {
+    return `The ${this.url} is opened`;
+  }
+}
 
 module.exports = {
   Header,
